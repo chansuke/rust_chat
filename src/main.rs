93031b9c3d58bf86a03079dd194dcf28d89cf031
chansuke <moonset20@gmail.com>
2016-07-10 01:45:21 +0900

    Start implement handshake

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,50 @@
 extern crate mio;
 extern crate http_muncher;
+extern crate sha1;
+extern crate rustc_serialize;
 
+use rustc_serialize::base64::{ToBase64, STANDARD};
 use http_muncher::{Parser, ParserHandler};
 use mio::*;
 use std::net::SocketAddr;
 use std::collections::HashMap;
 use mio::tcp::*;
+use std::cell::RefCell;
+use std::rc::Rc;
 
-struct HttpParser;
-impl ParserHandler for HttpParser {}
+fn gen_key(key: &String) -> String {
+  let mut m = sha1::Sha1::new()
+  let mut buf = [0u8; 20];
+
+  m.update(key.as_bytes());
+  m.update("258EAFA5-E914-47DA-95CA-C5AB0DC85B11".as_bytes());
+
+  m.output(&mut buf);
+
+  return buf.to_base64(STANDARD);
+}
+
+struct HttpParser {
+  current_key: Option<String>,
+  headers: Rc<RefCell<HashMap<String, String>>>
+}
+
+impl ParserHandler for HttpParser {
+  fn on_header_field(&mut self, s:&[u8]) -> bool {
+    self.current_key = Some(std::str::from_utf8(s).unwrap().to_string());
+  }
+
+  fn on_header_value(&mut self, s:&[u8]) -> bool {
+    self.headers.borrow_mut()
+      .insert(self.current_key.clone(.unwrap(),
+            std::str::from_utf8(s).unwrap().to_string());
+    true
+  }
+
+  fn on_header_complete(&mut self) -> bool {
+    false
+  }
+}
 
 struct WebSocketClient {
   socket: TcpStream,
