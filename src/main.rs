05864220a19103feac9a866761113d644e4aaa32
chansuke <moonset20@gmail.com>
2016-07-10 00:49:10 +0900

    Add http parser

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,49 @@
 extern crate mio;
-use mio::*;
+extern crate http_muncher;
 
+use http_muncher::{Parser, ParserHandler};
+use mio::*;
 use std::net::SocketAddr;
 use std::collections::HashMap;
 use mio::tcp::*;
+
+struct HttpParser;
+impl ParserHandler for HttpParser {}
+
+struct WebSocketClient {
+  socket: TcpStream,
+  http_parser: Parser<HttpParser>
+}
+
+impl WebSocketClient {
+  fn read(&mut self) {
+    loop {
+      let mut buf = [0; 2048];
+      match self.socket.try_read(&mut buf) {
+        Err(e) => {
+          println("Error while reading socket: {:?}", e);
+          return
+        },
+        Ok(None) =>
+          break,
+        Ok(Some(len)) => {
+          self.http_parser.parse(&buf[0..len]);
+          if self.http_parser.is_upgrade() {
+          // ...
+             break;
+          }
+        }
+      }
+    }
+  }
+
+  fn new(socket: TcpStream) -> WebSocketClient {
+    WebSocketClient {
+      socket: socket,
+      http_parser: Parser::request(HttpParser)
+    }
+  }
+}
 
 struct WebSocketServer {
   socket: TcpListener,
@@ -17,28 +57,32 @@ impl Handler for WebSocketServer {
     type Timeout = usize;
     type Message = ();
 
-    fn ready(&mut self, event_loop: &mut EventLoop<WebSocketServer>,
-             token: Token, events: EventSet)
-    {
+    fn ready(&mut self, event_loop: &mut EventLoop<WebSocketServer>, token: Token, events: EventSet) {
         match token {
-            SERVER_TOKEN => {
-                let client_socket = match self.socket.accept() {
-                    Err(e) => {
-                        println!("Accept error: {}", e);
-                        return;
-                    },
-                    Ok(None) => unreachable!("Accept has returned 'None'"),
-                    Ok(Some((sock, addr))) => sock
-                };
+          SERVER_TOKEN => {
+            let client_socket = match self.socket.accept() {
+              Err(e) => {
+                  println!("Accept error: {}", e);
+                  return;
+              },
+              Ok(None) => unreachable!("Accept has returned 'None'"),
+              Ok(Some((sock, addr))) => sock
+            };
 
-                self.token_counter += 1;
-                let new_token = Token(self.token_counter);
+            self.token_counter += 1;
+            let new_token = Token(self.token_counter);
 
-                self.clients.insert(new_token, client_socket);
-                event_loop.register(&self.clients[&new_token],
-                                    new_token, EventSet::readable(),
-                                    PollOpt::edge() | PollOpt::oneshot()).unwrap();
-            }
+            self.clients.insert(new_token, WebSocketClient::new(client_socket));
+            event_loop.register(&self.clients[&new_token].socket, new_token, EventSet::readable(),
+                        PollOpt::edge() | PollOpt::oneshot()).unwrap();
+
+          },
+          token => {
+            let mut client = self.clients.get_mut(&otken).unwrap();
+            client.read();
+            event_loop.register(&client.socket, token, EventSet::readable(),
+                          PollOpt::edge() | PollOpt::oneshot().unwrap();
+          }
         }
     }
 }
