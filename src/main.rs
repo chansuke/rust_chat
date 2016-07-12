cf75436caa4eed9788ce0a4075fc92b6aea2325a
chansuke <moonset20@gmail.com>
2016-07-13 03:39:36 +0900

    Finish reply frames

--- src/main.rs
+++ src/main.rs
@@ -142,7 +142,8 @@ impl WebSocketClient {
                     },
                     Err(e) => println!("error while reading frame: {}", e)
                 }
-            }
+            },
+            _ => {}
         }
     }
 
